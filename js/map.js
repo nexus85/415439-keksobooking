4fcbd7c2e74687e8dd656dbe4f8903dfb6317b5f
Klackky <natalia.volchatova@gmail.com>
2017-12-13 14:12:01 -0500

    метка до активации больше не перетаскивается

--- js/map.js
+++ js/map.js
@@ -29,6 +29,61 @@
     userPopup.classList.remove('map--faded');
     window.createPins(newAd, generateCard);
     window.form.activateForm();
+    // drag and drop!!!
+    mainPin.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        var coordinateY = mainPin.offsetTop - shift.y;
+        var coordinateX = mainPin.offsetLeft - shift.x;
+
+        // sets Y boundries
+        if (coordinateY < locationY.min) {
+          mainPin.style.top = locationY.min + 'px';
+        } else if (coordinateY > locationY.max) {
+          mainPin.style.top = locationY.max + 'px';
+        } else {
+          mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+        }
+
+        // sets X boundries
+        if (coordinateX < locationX.min) {
+          mainPin.style.left = locationX.min + 'px';
+        } else if (coordinateX > locationX.max) {
+          mainPin.style.left = locationX.max + 'px';
+        } else {
+          mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+        }
+        // coordinates goes to input 'address'
+        addressInput.value = 'x: ' + (coordinateX + MAIN_PIN_WIDTH / 2) + ', y: ' + (coordinateY + MAIN_PIN_HEIGHT + ARROW_HEIGHT);
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
   }
   // form and map activates on mouseup
   mainPin.addEventListener('mouseup', activateMap);
@@ -38,62 +93,6 @@
     if (event.keyCode === ENTER_KEYCODE) {
       activateMap();
     }
-  });
-
-  // drag and drop!!!
-  mainPin.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      var coordinateY = mainPin.offsetTop - shift.y;
-      var coordinateX = mainPin.offsetLeft - shift.x;
-
-      // sets Y boundries
-      if (coordinateY < locationY.min) {
-        mainPin.style.top = locationY.min + 'px';
-      } else if (coordinateY > locationY.max) {
-        mainPin.style.top = locationY.max + 'px';
-      } else {
-        mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
-      }
-
-      // sets X boundries
-      if (coordinateX < locationX.min) {
-        mainPin.style.left = locationX.min + 'px';
-      } else if (coordinateX > locationX.max) {
-        mainPin.style.left = locationX.max + 'px';
-      } else {
-        mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
-      }
-      // coordinates goes to input 'address'
-      addressInput.value = 'x: ' + (coordinateX + MAIN_PIN_WIDTH / 2) + ', y: ' + (coordinateY + MAIN_PIN_HEIGHT + ARROW_HEIGHT);
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
   });
 
 })();
