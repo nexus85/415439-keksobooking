7bee504e3793012f7ba578c158814f06d072bcf7
Keks <keks@htmlacademy.ru>
2017-12-15 01:00:47 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/card.js
+++ 7/js/card.js
@@ -2,6 +2,9 @@
 (function () {
   var template = document.querySelector('template').content.querySelector('.map__card');
   var card = document.body.appendChild(template.cloneNode(true)); // new card element from template
+  var before = document.querySelector('.map__filters-container');
+  var nodeParent = before.parentNode;
+  nodeParent.insertBefore(card, before); // inserts card before .map__filters-container:
 
   window.card = {
     card: card
