3e3fe2bfa060a3ef92ff4b9cf1f343da81b14180
Klackky <natalia.volchatova@gmail.com>
2017-12-15 12:56:26 -0500

    функция синхронизации поправлена

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,7 +3,7 @@
 (function () {
 
   window.synchronizeFields = function (mainField, dependentField, mainValues, dependentValues, syncFunction) {
-    var mainIndex = mainValues.indexOf(event.currentTarget.value); // index of current field
+    var mainIndex = mainValues.indexOf(mainField.value); // index of current field
     var dependentElement = dependentValues[mainIndex]; // index of dependentValue equals to main index.
     syncFunction(dependentField, dependentElement); // callback
   };
